c4c11340b9b801c112eb3bc8c79e286bd9c4e7c8
Nimrodell <margoha885@gmail.com>
2024-01-14 19:56:33 +0300

    did HomeTask

--- HomeTask/Program.cs
+++ HomeTask/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 8;
+double b = 4.2;
+int c = 2;
+double d = 1.5;
+
+double r1= a * b;
+double r2 = c + d;
+double r3 = r1/r2;
+Console.WriteLine(r3);
