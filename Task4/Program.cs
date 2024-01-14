a4c1211366a1d34b61d5ca58430aa3d375cfa866
Nimrodell <margoha885@gmail.com>
2024-01-14 22:12:16 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 7;
+int i = 9;
+int d = 41;
+int e = 10;
+
+int max = a;
+
+if (b > max) ;
+{
+    max = b;
+}
+if (i > max) ;
+{
+    max = i;
+}
+if (d > max) ;
+{
+    max = d;
+}
+if (e > max) ;
+{
+    max = e;
+}
+Console.WriteLine(max);
